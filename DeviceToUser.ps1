7e987ea6f4626f68354d29110c2a1f95307941d2
Alan Sonnheim <alan.sonnheim@thermtech.net>
2026-07-28 10:19:33 -0500

    Bind local user to computer

--- DeviceToUser.ps1
+++ DeviceToUser.ps1
@@ -19,54 +19,46 @@ ForEach ($Line in $EnvLines) {
 	$Env.Add($Name, $Value)
 }
 
-$Raw = @(Invoke-Command -ComputerName $Env["REMOTE_COMPUTER"] -ScriptBlock { 
-    Get-EventLog -LogName Security -Newest 10000 | Where-Object { $_.EventID -eq 4769 } | Select-Object -ExpandProperty Message # Use EventID 4624 instead?
-})
+$Raw = New-Object System.Collections.Generic.List[System.Object]
+
+ForEach ($DC in $Env["REMOTE_COMPUTERS"] -split ',') {
+	$CDC = Invoke-Command -ComputerName $DC -ScriptBlock {
+		Get-WinEvent -FilterHashtable @{ LogName = 'Security'; Id = 4769 } | Select-Object Message
+	}
+	
+	ForEach ($Item in $CDC) {
+		$Raw.Add($Item)
+	}
+}
 
 $AccountNames = New-Object System.Collections.Generic.List[System.Object]
 $DeviceNames = New-Object System.Collections.Generic.List[System.Object]
 $UserDeviceMap = @{ }
-$UserNames = @(Get-ADUser -Filter * -SearchBase $Env["SEARCH_BASE"] | Select-Object -ExpandProperty SamAccountName)
+$UserNames = @(Get-ADUser -Filter * -SearchBase $Env["USER_BASE"] | Select-Object -ExpandProperty SamAccountName)
+$ComputerNames = @(Get-ADComputer -Filter * -SearchBase $Env["COMPUTER_BASE"] | Select-Object -ExpandProperty Name)
 
-ForEach ($Output in $Raw) {
-	$I = 82
-	While ($Output[$I] -ne "`t") {
-		$I++
-	}
+ForEach ($Output in $Raw) {	
+	$Output -match 'Account Name:\s*(\S+)' > $Null
+	$AccountName = (($Matches[1] -split '@')[0]).Replace('$', '')
 	
-	$AccountName = $Output.SubString(82, $I - 82)
-	
-	$J = $Output.IndexOf("Service Name:") + 15
-	While ($Output[$J] -ne "`t") {
-		$J++
-	}
-	
-	$DeviceName = $Output.SubString($Output.IndexOf("Service Name:") + 15, $J - ($Output.IndexOf("Service Name:") + 15)).Replace('$', '')
+	$Output -match 'Service Name:\s*(\S+)' > $Null
+	$DeviceName = $Matches[1].Replace('$', '')
 	
 	$List = New-Object System.Collections.Generic.List[System.Object]
 	
-	$FormattedName = (($AccountName -split '@')[0]).Replace('$', '')
-	
-	If ((-not ($UserDeviceMap[$DeviceName] -contains $FormattedName)) -and ($UserNames -contains $FormattedName)) {
-		If (-not ($UserDeviceMap.Keys -contains $DeviceName)) {
-			$UserDeviceMap.Add($DeviceName, $List)
-		}
-		$UserDeviceMap[$DeviceName].Add($FormattedName)
+	If (($UserDeviceMap.Keys -notcontains $DeviceName) -and ($ComputerNames -contains $DeviceName)) {
+		$UserDeviceMap.Add($DeviceName, $List)
 	}
-}
-
-$FilteredAccountNames = New-Object System.Collections.Generic.List[System.Object]
-
-ForEach ($AccountName in $AccountNames) {
-	If ($UserNames -contains $FormattedName -and $FormattedName -ne "`r`n") {
-		$FilteredAccountNames.Add($FormattedName)
+	
+	If (($UserNames -contains $AccountName) -and ($UserDeviceMap.Keys -contains $DeviceName) -and ($UserDeviceMap[$DeviceName] -notcontains $AccountName)) {
+		$UserDeviceMap[$DeviceName].Add($AccountName)
 	}
 }
 
 ForEach ($Device in $UserDeviceMap.Keys) {
-	Write-Host " "
-	Write-Host "----- DEVICE: $($Device)" -NoNewline
+	Add-Content -Path .\test.txt -Value " "
+	Add-Content -Path .\test.txt -Value "----- DEVICE: $($Device)"
 	ForEach ($User in $UserDeviceMap[$Device]) {
-		Write-Host $User
+		Add-Content -Path .\test.txt -Value $User
 	}
 }
